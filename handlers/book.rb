7f9a2be2ca8106fb760b33d937029a7e9199a889
Devin Gaffney <itsme@devingaffney.com>
2012-12-08 17:55:12 -0800

    woooooooo

--- handlers/book.rb
+++ handlers/book.rb
@@ -4,3 +4,20 @@ get "/books" do
   @books = Book.all
   erb :"books/index"
 end
+
+get "/books/new" do
+  erb :"books/new"
+end
+
+post "/books/new" do
+  puts params.inspect
+  @book = Book.new
+  @book.title = params[:title]
+  @book.number_of_pages = params[:number_of_pages]
+  puts @book.inspect
+  @book.save
+  erb :"books/index"
+end
+
+# self.erb(:"some/path/to/a/file") => Go look in the views folder, find the file matching
+# the path that was sent in, and start filling in all the blanks where there are <% %> tags.
