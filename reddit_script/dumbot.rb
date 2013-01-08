36ff6dfa7131ada8b6749b437f00d037c0eaa988
hamburgerG <adam.k.jet@gmail.com>
2013-01-08 06:32:27 -0800

    bare title-reading script

---
+++ reddit_script/dumbot.rb
@@ -0,0 +1,15 @@
+require 'open-uri'
+require 'json'
+
+front_page = JSON.parse(open("http://www.reddit.com/.json").read)
+
+i = 0
+
+puts "\n\n"
+
+
+for item in front_page["data"]["children"].collect{|x| x["data"]["title"]}
+   puts "      #{i}       #{item} \n\n"
+
+   i += 1
+end
