1ccfd1f6f338004ba3949fe93c316a0f64e5969f
hamburgerG <adam.k.jet@gmail.com>
2013-01-08 06:34:52 -0800

    Update reddit_script/dumbot.rb

--- reddit_script/dumbot.rb
+++ reddit_script/dumbot.rb
@@ -2,11 +2,9 @@ require 'open-uri'
 require 'json'
 
 front_page = JSON.parse(open("http://www.reddit.com/.json").read)
-
 i = 0
 
 puts "\n\n"
-
 
 for item in front_page["data"]["children"].collect{|x| x["data"]["title"]}
    puts "      #{i}       #{item} \n\n"
